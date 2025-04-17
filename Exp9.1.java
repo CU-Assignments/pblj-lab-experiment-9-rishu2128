ef925fa4a76ae09add43c4527759c16735d87775
Rishant <rishant7870@gmail.com>
2025-04-17 12:38:50 +0530

    Update Exp9.1.java

--- Exp9.1.java
+++ Exp9.1.java
@@ -1,1 +1,52 @@
 
+public class Course {
+    private String courseName;
+    private int duration; // in weeks
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
+
+public class Student {
+    private String name;
+    private Course course;
+
+    public Student(String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Enrolled Course: " + course.getCourseName());
+        System.out.println("Course Duration: " + course.getDuration() + " weeks");
+    }
+}
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Course course() {
+        return new Course("Spring Framework", 6);
+    }
+
+    @Bean
+    public Student student() {
+        return new Student("Sameer", course());
+    }
+}
